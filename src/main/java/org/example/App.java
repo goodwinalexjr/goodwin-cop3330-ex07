3cc5822aa62d605c831079b83eeb8df75383f5eb
Alex Goodwin <goodwinalexjr@knights.ucf.edu>
2021-05-31 20:13:40 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner numbers = new Scanner(System.in);
+        System.out.print("What is the length of the room in feet? ");
+        int a = numbers.nextInt();
+        System.out.print("What is the width of the room in feet? ");
+        int b = numbers.nextInt();
+        System.out.println("You entered diensions of " + a + " feet by " + b + " feet.");
+        System.out.println("The area is");
+        int c  = a * b;
+        System.out.println(c + " square feet");
+        double d = c * 0.09290304;
+        DecimalFormat mform = new DecimalFormat("#.000");
+        System.out.println(mform.format(d) + " square meters");
+    }
+}
